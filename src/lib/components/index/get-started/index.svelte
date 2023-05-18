c84e4d653ca09e80c24e1a7f901fde2542b6d363
Siddhant Khare <siddhant@gitpod.io>
2023-05-18 13:05:32 +0000

    initial commit

--- src/lib/components/index/get-started/index.svelte
+++ src/lib/components/index/get-started/index.svelte
@@ -3,16 +3,22 @@
   import LaunchExampleWorkspace from "./launch-example-workspace.svelte";
   import LinkGitRepository from "./link-git-repository.svelte";
   import Card from "$lib/components/ui-library/card";
+  import configCatClient from "$lib/utils/feature-flag-provider";
 
-  let checked: boolean = false;
+  var featureFlag = true;
+  configCatClient
+    .getValueAsync("home_example_launch_workspaces_card", false)
+    .then((value) => {
+      featureFlag = value;
+    });
 </script>
 
 <Section id="get-started" class="pt-20">
   <h2 class="!mb-x-small text-center text-h2">Get started for free</h2>
   <div
     class="cards-container flex flex-col lg:flex-row max-w-[600px] lg:max-w-none gap-4 mx-auto"
-    class:justify-center={checked}
-    class:justify-between={!checked}
+    class:justify-between={featureFlag}
+    class:justify-center={!featureFlag}
   >
     <Card
       class="p-xx-small sm:py-small basis-[50%] sm:px-x-small xl:p-medium text-center"
@@ -20,11 +26,14 @@
     >
       <LinkGitRepository />
     </Card>
-    <Card
-      class="p-xx-small sm:py-small  basis-[50%] sm:px-x-small xl:p-medium text-center"
-      size="small"
-    >
-      <LaunchExampleWorkspace />
-    </Card>
+
+    {#if featureFlag}
+      <Card
+        class="p-xx-small sm:py-small  basis-[50%] sm:px-x-small xl:p-medium text-center"
+        size="small"
+      >
+        <LaunchExampleWorkspace />
+      </Card>
+    {/if}
   </div>
 </Section>
---
+++ src/lib/utils/feature-flag-provider.ts
@@ -0,0 +1,6 @@
+import * as configcat from "configcat-js-ssr";
+const configCatClient = configcat.getClient(
+  "OSzbCCd5YESN3ZtcqYIpkA/n8MSGCfKLUC2OcnKX0i2ZQ"
+);
+
+export default configCatClient;
