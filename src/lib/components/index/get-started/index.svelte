af922c41a0f3f6e2d2694e1b8d16722e6063d799
Siddhant Khare <siddhant@gitpod.io>
2023-05-19 04:47:05 +0000

    Better code structure & code comments

--- src/lib/components/index/get-started/index.svelte
+++ src/lib/components/index/get-started/index.svelte
@@ -3,22 +3,27 @@
   import LaunchExampleWorkspace from "./launch-example-workspace.svelte";
   import LinkGitRepository from "./link-git-repository.svelte";
   import Card from "$lib/components/ui-library/card";
-  import configCatClient from "$lib/utils/feature-flag-provider";
+  import { getFeatureFlag } from "$lib/utils/feature-flag-provider";
 
-  var featureFlag = true;
-  configCatClient
-    .getValueAsync("home_example_launch_workspaces_card", false)
-    .then((value) => {
-      featureFlag = value;
-    });
+  /* The exampleFlagValue variable stores the value of the home_example_launch_workspaces_card feature flag.
+   * The getFeatureFlag() function retrieves the value of the specified feature flag.
+   * The feature flag is set to false by default, but is overridden by the value of the feature flag stored in local storage.
+   * The value of the feature flag is also set to the value of the exampleFlagValue variable.
+   */
+
+  let exampleFlagValue = getFeatureFlag(
+    "home_example_launch_workspaces_card",
+    false,
+    (val) => (exampleFlagValue = val)
+  );
 </script>
 
 <Section id="get-started" class="pt-20">
   <h2 class="!mb-x-small text-center text-h2">Get started for free</h2>
   <div
     class="cards-container flex flex-col lg:flex-row max-w-[600px] lg:max-w-none gap-4 mx-auto"
-    class:justify-between={featureFlag}
-    class:justify-center={!featureFlag}
+    class:justify-between={exampleFlagValue}
+    class:justify-center={!exampleFlagValue}
   >
     <Card
       class="p-xx-small sm:py-small basis-[50%] sm:px-x-small xl:p-medium text-center"
@@ -27,7 +32,7 @@
       <LinkGitRepository />
     </Card>
 
-    {#if featureFlag}
+    {#if exampleFlagValue}
       <Card
         class="p-xx-small sm:py-small  basis-[50%] sm:px-x-small xl:p-medium text-center"
         size="small"
--- src/lib/utils/feature-flag-provider.ts
+++ src/lib/utils/feature-flag-provider.ts
@@ -1,6 +1,29 @@
 import * as configcat from "configcat-js-ssr";
+import { getOrSetCookieId } from "$lib/components/segment.svelte";
+
 const configCatClient = configcat.getClient(
   "OSzbCCd5YESN3ZtcqYIpkA/n8MSGCfKLUC2OcnKX0i2ZQ"
 );
+const userId = getOrSetCookieId();
 
-export default configCatClient;
+type GetFeatureFlagDone = {
+  (flagValue: boolean): void;
+};
+
+// getFeatureFlag function retrieves the value of a feature flag from the ConfigCat API.
+
+export const getFeatureFlag = (
+  flagName: string,
+  defaultValue: boolean,
+  done: GetFeatureFlagDone
+): boolean => {
+  configCatClient
+    .getValueAsync(flagName, defaultValue, new configcat.User(userId))
+    .then(done)
+    .catch((error) => {
+      console.error(error);
+    });
+
+  // return the default value until the feature flag value is fetched
+  return defaultValue;
+};
